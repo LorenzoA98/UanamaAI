4ba94326b5ec698581f98216c1828ae9dd044908
Lorenzo Maturo <lorenzomaturo98@gmail.com>
2023-02-15 06:24:15 +0100

    update server

--- client/script.js
+++ client/script.js
@@ -89,7 +89,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     try {
-        const response = await fetch('http://localhost:5000', {
+        const response = await fetch('https://uanamaai.onrender.com', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
